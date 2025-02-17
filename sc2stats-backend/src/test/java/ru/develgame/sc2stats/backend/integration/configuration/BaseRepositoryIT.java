a67c7fdee48f45fddc381b4a4ab2000b7bf79f41
izemskov <pascal.ilya@gmail.com>
2025-02-18 00:28:33 +0100

    Remove unnecessary test spring extension

--- sc2stats-backend/src/test/java/ru/develgame/sc2stats/backend/integration/configuration/BaseRepositoryIT.java
+++ sc2stats-backend/src/test/java/ru/develgame/sc2stats/backend/integration/configuration/BaseRepositoryIT.java
@@ -1,13 +1,10 @@
 package ru.develgame.sc2stats.backend.integration.configuration;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Import(TestDBConfiguration.class)
 @TestPropertySource(locations = "/application-test.yml")
--- sc2stats-backend/src/test/java/ru/develgame/sc2stats/backend/integration/configuration/BaseRestControllerIT.java
+++ sc2stats-backend/src/test/java/ru/develgame/sc2stats/backend/integration/configuration/BaseRestControllerIT.java
@@ -1,18 +1,15 @@
 package ru.develgame.sc2stats.backend.integration.configuration;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Import({TestDBConfiguration.class,
         TestServiceConfiguration.class,
