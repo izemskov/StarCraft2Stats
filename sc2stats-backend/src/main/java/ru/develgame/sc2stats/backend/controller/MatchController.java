a91f73db35eea40f8340b3253918eea012cd99d7
izemskov <pascal.ilya@gmail.com>
2025-05-13 15:47:29 +0200

    Get matches by pages backend

--- sc2stats-backend/src/main/java/ru/develgame/sc2stats/backend/controller/MatchController.java
+++ sc2stats-backend/src/main/java/ru/develgame/sc2stats/backend/controller/MatchController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.develgame.sc2stats.backend.dto.MatchResponseDto;
+import ru.develgame.sc2stats.backend.bean.MatchesPage;
+import ru.develgame.sc2stats.backend.dto.MatchesResponseDto;
 import ru.develgame.sc2stats.backend.dto.filter.MatchDecision;
 import ru.develgame.sc2stats.backend.dto.filter.MatchType;
 import ru.develgame.sc2stats.backend.mapper.MatchMapper;
 import ru.develgame.sc2stats.backend.service.MatchService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/sc2/match")
@@ -22,11 +21,15 @@ public class MatchController {
     private final MatchMapper matchMapper;
 
     @GetMapping
-    public ResponseEntity<List<MatchResponseDto>> fetchAll(@RequestParam(required = false) MatchType type,
-                                                           @RequestParam(required = false) MatchDecision decision) {
+    public ResponseEntity<MatchesResponseDto> fetchAll(@RequestParam(required = false) MatchType type,
+                                                       @RequestParam(required = false) MatchDecision decision,
+                                                       @RequestParam(defaultValue = "0") int page,
+                                                       @RequestParam(defaultValue = "25") int size) {
+        MatchesPage matchesPage = matchService.fetchAllMatchesSortedByDateDesc(type, decision, page, size);
 
-        return ResponseEntity.ok(matchService.fetchAllMatchesSortedByDateDesc(type, decision).stream()
-                .map(matchMapper::toDto)
-                .toList());
+        return ResponseEntity.ok(MatchesResponseDto.builder()
+                .total(matchesPage.total())
+                .matches(matchesPage.matches().stream().map(matchMapper::toDto).toList())
+                .build());
     }
 }
