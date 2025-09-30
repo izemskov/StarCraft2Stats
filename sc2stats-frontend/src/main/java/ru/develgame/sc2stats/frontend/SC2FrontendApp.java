84cdd4fd7d9dd0357ed76f9e1d02081d74f0ab6b
izemskov <pascal.ilya@gmail.com>
2025-09-30 22:55:14 +0300

    Refactoring

--- sc2stats-frontend/src/main/java/ru/develgame/sc2stats/frontend/SC2FrontendApp.java
+++ sc2stats-frontend/src/main/java/ru/develgame/sc2stats/frontend/SC2FrontendApp.java
@@ -3,11 +3,9 @@ package ru.develgame.sc2stats.frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableScheduling
 public class SC2FrontendApp {
     public static void main(String[] args) {
         SpringApplication.run(SC2FrontendApp.class, args);
